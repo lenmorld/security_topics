d3aea06ef3c1f1139f8519b38e12fc58a3bf3cf1
lenmorld <lenmorld@gmail.com>
2023-08-15 18:01:58 -0400

    part 1 - server1 authentication with JWT

---
+++ jwt_auth/README.md
@@ -0,0 +1,15 @@
+https://www.youtube.com/watch?v=mbsmsi7l3r4
+
+
+JWT 
+
+## part 1 - authentication
+
+```
+npm start
+```
+this starts server 1 on http://localhost:3000
+
+- on successful login, create JWT, by serializing user info with a secret
+- authenticateToken middleware for protected requests
+- verify token exist and valid using secret
---
+++ jwt_auth/server.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+const PORT = 3000
+
+// based on body-parser 
+app.use(express.json())
+
+const posts = [
+    {
+        username: 'Lenny',
+        title: 'Post 1'
+    },
+    {
+        username: 'Jimmy',
+        title: 'Post 2'
+    }
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+    // req.user should be defined if
+    // middleware reached here
+    res.json(posts.filter(post => post.username === req.user.name ))
+})
+
+app.post('/login', (req, res) => {
+    // auth user
+
+    const username = req.body.username
+    const user = { name: username }
+
+    // create JWT
+    // serialize user object with a secret
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken })
+
+})
+
+// authenticate middleware
+function authenticateToken(req, res, next) {
+    // verify that this is a user
+
+    // get token from header
+    // Authorization: Bearer TOKEN
+
+    const authHeader = req.headers['authorization']
+    const token = authHeader?.split(' ')[1]
+
+    if (!token) {
+        // no credentials
+        return res.sendStatus(401)
+    }
+
+    // verify token
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,
+        (err, user) => {
+            if (err) {
+                // invalid credentials
+                res.sendStatus(401)
+            }
+            req.user = user
+            next()
+        })
+
+}
+
+app.listen(PORT)
